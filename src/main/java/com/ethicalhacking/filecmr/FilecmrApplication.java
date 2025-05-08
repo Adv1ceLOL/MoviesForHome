5f9e18a5511cdfe44ccade2cbe28768f3865cab0
Adv1ceLOL <nerisdisanto.1985793@studenti.uniroma1.it>
2025-05-08 08:45:52 +0200

    small fixes

--- src/main/java/com/ethicalhacking/filecmr/FilecmrApplication.java
+++ src/main/java/com/ethicalhacking/filecmr/FilecmrApplication.java
@@ -2,19 +2,11 @@ package com.ethicalhacking.filecmr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class FilecmrApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-	System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-	System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-	System.setProperty("DB_NAME", dotenv.get("DB_DATABASE"));
-	System.setProperty("DB_USERNAME", dotenv.get("DB_USER"));
-	System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
 		SpringApplication.run(FilecmrApplication.class, args);
 	}
 
